eb859325f291777bdff57a05f29252041596fa1f
Paras Sharma <paras16jul1991@Parass-MacBook-Air.local>
2021-04-23 11:55:48 +0530

    kafka server files added.

---
+++ kafka/kafka-server/start.sh
@@ -0,0 +1,3 @@
+java -jar ~/softwares/kafka-server/kafdrop-3.27.0.jar;  
+~/softwares/kafka-server/kafka_2.12-2.6.0/bin/zookeeper-server-start.sh ~/softwares/kafka-server/kafka_2.12-2.6.0/config/zookeeper.properties;
+~/softwares/kafka-server/kafka_2.12-2.6.0/bin/kafka-server-start.sh ~/softwares/kafka-server/kafka_2.12-2.6.0/config/server.properties;
